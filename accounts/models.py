4c789fe4ff648d809eb9b7c0c76379d5239c9f58
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-06-13 17:39:32 +0300

    update User migrations

--- accounts/models.py
+++ accounts/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id_number = models.CharField(max_length=55, null=True, blank=True)
-    avatar = models.ImageField(upload_to='avatar-images/', default=None)
-
+    avatar = models.ImageField(upload_to='avatar-images/', null=True, blank=True)
