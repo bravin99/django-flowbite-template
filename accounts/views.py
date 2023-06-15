837bb988165a1c3783d6619a641c59f1cf86e9f3
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-06-15 22:02:08 +0300

    update user creation form being used

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import  CreateView
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
