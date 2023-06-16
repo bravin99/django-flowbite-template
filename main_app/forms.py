c98fa2646a2437c119c234c6454c0bd125d09594
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-06-16 21:21:59 +0300

    add model in Contact form

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,5 +4,6 @@ from main_app.models import Contact
 
 class ContactForm(forms.ModelForm):
     class Meta:
+        model = Contact
         fields = ['full_name', 'email', 'phone', 'message', 'call_back']
 
