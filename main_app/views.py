7244488474fb1f893c5ead0daf9fa463ea328be2
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-06-16 21:15:35 +0300

    contact us view

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from main_app.forms import ContactForm
+from django.contrib import messages
 
 
 def landing_page(request):
     return render(request, 'core/landing.html')
+
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your message was sent")
+        else:
+            messages.error(request, "Error in submitted message")
+    else:
+        form = ContactForm()
+
+    return render(request, 'main_app/contact.html', context = {
+        'form': form,
+    })
+
