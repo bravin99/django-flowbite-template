d082cbcb726a918ad7ed64249f3bc1753999cd38
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-06-16 20:50:29 +0300

    create base model for main app

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class MainAppBaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
