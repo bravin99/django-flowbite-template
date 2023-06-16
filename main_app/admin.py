d0bf73a291697b534ba52d7e78882ce88db89036
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-06-16 21:50:05 +0300

    register contact in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from main_app.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone', 'call_back', 'created']
+    list_filter = ['call_back', 'created', 'updated']
+    search_fields = ['full_name', 'email', 'phone']
+
