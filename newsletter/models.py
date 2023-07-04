90ef3fb94304147ad06ec322115e5c706047680a
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-07-04 21:16:39 +0300

    Subscriber model

--- newsletter/models.py
+++ newsletter/models.py
@@ -5,3 +5,13 @@ class NewsletterBaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+
+class Subscriber(NewsletterBaseModel):
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    receive_emails = models.BooleanField(default=True)
+    confirmed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
