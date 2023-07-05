1e346f0d49ac4aa8496e73a84a079091ab1fd36c
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-07-05 20:49:15 +0300

    subscribe view

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect
+from newsletter.models import Subscriber, Newsletter
+from django.contrib import messages
 
-# Create your views here.
+
+def subscribe(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        name = request.POST['full_name']
+
+        if Subscriber.objects.filter(email=email).exists():
+            messages.warning(request, "Email is already registered")
+        else:
+            Subscriber.objects.create(email=email, name=name)
+            messages.success(request, "Your subscription was successful")
+
+    return redirect(request.META.get('HTTP_REFERER'))
