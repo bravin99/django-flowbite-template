343e82915c3d77e1a66c6d2248c428b6c0fee189
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-07-06 21:37:13 +0300

    link subscriber confirmation url

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,5 +3,6 @@ from newsletter import views
 
 urlpatterns = [
     path('subscribe', views.subscribe, name='subscribe'),
-    path('unsubscribe', views.unsubscribe, name='unsubscribe')
+    path('unsubscribe', views.unsubscribe, name='unsubscribe'),
+    path('confirm-email/<slug:confirmation_id>', views.confirmation, name='confirm_newsletter_email'),
 ]
