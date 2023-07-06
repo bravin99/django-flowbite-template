c9fab42a55d530d1c72132b8a91e537973f7c445
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-07-06 21:28:36 +0300

    send email when a client subscribes

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, HttpResponseRedirect, redirect
 from newsletter.models import Subscriber, Newsletter
 from django.contrib import messages
 import uuid
+from smtplib import SMTPException, SMTPAuthenticationError, SMTPServerDisconnected
+from socket import error as SocketError
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def subscribe(request):
@@ -15,7 +20,21 @@ def subscribe(request):
             confirmation_id = uuid.uuid4()
             Subscriber.objects.create(email=email, name=name,
                                       confirmation_id=confirmation_id)
-            messages.success(request, "Your subscription was successful")
+            messages.success(request, "Your subscription was successful, "
+                                      "check your inbox for confirmation link")
+            try:
+                site = get_current_site(request)
+                email_subject = 'Email confirmation for our newsletter'
+                message = render_to_string('emails/confirm_newsletter_email.html', {
+                    'domain': site.domain,
+                    'confirm_code': confirmation_id
+                })
+                email_message = EmailMessage(email_subject, message, to=[email])
+                email_message.send()
+            except (SMTPException, SMTPAuthenticationError, SMTPServerDisconnected, SocketError) as exc:
+                messages.error(request, "Error sending email")
+                # TODO: log error
+                print(exc)
 
     return redirect(request.META.get('HTTP_REFERER'))
 
